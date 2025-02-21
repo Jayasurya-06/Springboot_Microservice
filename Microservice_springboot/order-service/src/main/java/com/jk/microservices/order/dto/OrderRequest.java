80e24a3dbc7ac9286b08d7a3ea68870d1879c945
jayasurya <jayasuryagowri97@gmail.con>
2025-02-21 23:50:54 +0530

    kafka implemendation

--- Microservice_springboot/order-service/src/main/java/com/jk/microservices/order/dto/OrderRequest.java
+++ Microservice_springboot/order-service/src/main/java/com/jk/microservices/order/dto/OrderRequest.java
@@ -2,9 +2,10 @@ package com.jk.microservices.order.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest( Long id,
-         String orderNumber,
-         String skuCode,
-         BigDecimal price,
-         Integer quality) {
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id, String orderNumber, String skuCode,
+                           BigDecimal price, Integer quantity, UserDetails userDetails) {
+
+    public record UserDetails(String email, String firstName, String lastName) {}
 }
--- Microservice_springboot/order-service/src/main/java/com/jk/microservices/order/service/OrderService.java
+++ Microservice_springboot/order-service/src/main/java/com/jk/microservices/order/service/OrderService.java
@@ -2,10 +2,12 @@ package com.jk.microservices.order.service;
 
 import com.jk.microservices.order.client.InventoryClient;
 import com.jk.microservices.order.dto.OrderRequest;
+import com.jk.microservices.order.event.OrderPlacedEvent;
 import com.jk.microservices.order.model.Order;
 import com.jk.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -20,17 +22,28 @@ public class  OrderService {
     @Autowired
     private final InventoryClient inventoryClient;
 
+    @Autowired
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
 
     public void placeOrder(OrderRequest orderRequest) {
 
-        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quality());
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         if(isInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price());
             order.setSkuCode(orderRequest.skuCode());
-            order.setQuality(orderRequest.quality());
+            order.setQuality(orderRequest.quantity());
             orderRepository.save(order);
+
+            // send message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+            kafkaTemplate.send("order-placed",orderPlacedEvent);
         }else{
             throw new RuntimeException("Product with skucode "+orderRequest.skuCode()+" is not in stack");
         }
