72caac10a0d118955500370a26d58717ea90de68
jayasurya <jayasuryagowri97@gmail.con>
2025-02-24 18:04:34 +0530

    GRAFANA AND LOKI IMPLEMENTED

--- Microservice_springboot/order-service/src/main/java/com/jk/microservices/order/config/ResetClientConfig.java
+++ Microservice_springboot/order-service/src/main/java/com/jk/microservices/order/config/ResetClientConfig.java
@@ -1,6 +1,8 @@
 package com.jk.microservices.order.config;
 
 import com.jk.microservices.order.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +15,30 @@ public class ResetClientConfig {
 
     @Value("${inventory.service.url}")
     private String inventoryUrl;
+    @Autowired
+    private final ObservationRegistry observationRegistry;
+
+    public ResetClientConfig(ObservationRegistry observationRegistry) {
+        this.observationRegistry = observationRegistry;
+    }
+
 
     @Bean
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryUrl)
+//                .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
+
+//    private ClientHttpRequestFactory getClientRequestFactory() {
+//        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+//                .withConnectTimeout(Duration.ofSeconds(3))
+//                .withReadTimeout(Duration.ofSeconds(3));
+//        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+//    }
 }
